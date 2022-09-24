3b41f280a42280edbf76dac0ac1da093273b8e7a
Нещётки Даниил <dneshotkin@gmail.com>
2022-09-24 15:24:37 +0300

    User can Register and Log In

---
+++ Shop/Controllers/UsersController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shop.Data;
+using Shop.Models;
+using Shop.ViewModels;
+
+namespace Shop.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly ShopContext _context;
+        private const string _userKey = "user";
+
+        private User? _user
+        {
+            get
+            {
+                int? userId = HttpContext.Session.GetInt32(_userKey);
+                return userId is null ? null : _context.Users.First(user => user.Id == userId);
+            }
+        }
+
+        public UsersController(ShopContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Details()
+        {
+            if (_user is null)
+            {
+                return RedirectToAction(nameof(Login));
+            }
+            return View(_user);
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(string email, string password)
+        {
+            User? user = await _context.Users.FirstOrDefaultAsync(user => user.Email == email && user.Password == password);
+            if (user is null)
+            {
+                return View();
+            }
+            HttpContext.Session.SetInt32(_userKey, user.Id);
+            HttpContext.Session.SetString("userName", user.Name);
+            return RedirectToAction(nameof(Index), controllerName: "Home");
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register([Bind("Name, Password, ConfirmPassword,Email,Phone")] UserViewModel userModel)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = new(userModel);
+                _context.Add(user);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index), controllerName: "Home");
+            }
+            return View(userModel);
+        }
+    }
+}
--- Shop/Models/User.cs
+++ Shop/Models/User.cs
@@ -1,16 +1,29 @@
-﻿using Shop.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using Shop.Data;
+using Shop.ViewModels;
+using System.ComponentModel.DataAnnotations;
 
 namespace Shop.Models
 {
+    [Index(nameof(Email), nameof(UserId), IsUnique = true)]
     public class User
     {
         public int Id { get; private set; }
+        [Required, StringLength(30)]
         public string Name { get; private set; }
+        [Display(Name = "User Id")]
         public Guid UserId { get; private set; }
+
+        [Required, RegularExpression("^.*(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!*+_-]).{8,}$",
+            ErrorMessage = "Password must contains at list 1 top and 1 small letters, 1 number and 1 symbol(!,*,+,_ or -)")]
         public string Password { get; private set; }
+        [Required, EmailAddress, Display(Prompt = "example@mail.com")]
         public string Email { get; private set; }
-        public string Phone { get; private set; }
+        [Phone, RegularExpression(@"^\d.{9}$", ErrorMessage = "Wrong phone format"), Display(Prompt = "0xxxxxxxx")]
+        public string? Phone { get; private set; }
+        public bool IsAdmin { get; private set; }
         public decimal Balance { get; private set; }
+        [Display(Name = "Money Spent")]
         public decimal MoneySpent { get; private set; }
         public List<Product> Cart { get; private set; }
         public List<History> History { get; private set; }
@@ -29,7 +42,29 @@ namespace Shop.Models
             Password = string.Empty;
         }
 
+        public User(UserViewModel userModel) : this()
+        {
+            Name = userModel.Name;
+            Email = userModel.Email;
+            Phone = userModel.Phone;
+            Password = userModel.Password;
+        }
 
+        public User(string name, string password) : this()
+        {
+            Name = name;
+            Password = password;
+        }
 
+        public User(string name, string password, string email, string? phone) : this(name, password)
+        {
+            Email = email;
+            Phone = phone;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
     }
 }
